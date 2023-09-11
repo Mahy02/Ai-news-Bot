4709d545efb81b0f3909728a5a411714bbd6d06d
Mahy02 <Mahinouralaa2002@gmail.com>
2023-09-11 12:43:40 +0300

    summary based on topics + fixed pytube error

--- integration.py
+++ integration.py
@@ -1,1 +1,5 @@
 # The flow: We have agent that uses google search tool for youtube videos and then load them into documents and extract the releveant topics
+# then we summarize
+# then we turn into a tweet
+
+#we also want to get the source info & other stuff 
